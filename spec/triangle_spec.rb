12cf46804e2977089964a603195dd37ec7651d22
Anna Valkov & Nuatu <student@epicodus.com>
2014-07-29 09:54:28 -0700

    test the class initialization

---
+++ spec/triangle_spec.rb
@@ -0,0 +1,15 @@
+require 'rspec'
+require 'triangle'
+
+describe Triangle do
+  it 'is initialized with three sides' do
+    test_triangle = Triangle.new(3,3,3)
+    test_triangle.should be_an_instance_of Triangle
+  end
+  it 'lets you read the three sides' do
+    test_triangle = Triangle.new(1,2,3)
+    test_triangle.side1.should eq 1
+    test_triangle.side2.should eq 2
+    test_triangle.side3.should eq 3
+  end
+end
